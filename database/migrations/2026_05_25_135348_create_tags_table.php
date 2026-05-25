a79149d850e66b28ffbcaa214b458dc2d5f2cbb6
sajadirc <sajad.jen@gmail.com>
2026-05-25 17:28:05 +0330

    tags table and Model Are implemneted

--- database/migrations/2026_05_25_135348_create_tags_table.php
+++ database/migrations/2026_05_25_135348_create_tags_table.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
-            
+            $table->string('name');
+            $table->string('persian_name');
             $table->timestamps();
         });
     }
