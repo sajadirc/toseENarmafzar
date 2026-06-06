b84eb8f01abfd62af791fc48ae56d5419d7a609f
sajadirc <sajad.jen@gmail.com>
2026-06-06 18:19:08 +0330

    add Divider for admin sideBar and show Admin Role in panel

--- resources/views/layouts/panel/sideBar.blade.php
+++ resources/views/layouts/panel/sideBar.blade.php
@@ -1,9 +1,10 @@
-<div class="right-menu">
+<div class="right-menu border-end border-light">
     {{-- Top logout Btn --}}
-    <div class="d-flex justify-content-between m-2">
+    <div class="d-flex justify-content-around m-2">
         <h1 class="fs-4 text-center">پنل <span class="text-danger">ادمین</span></h1>
     <a href="{{route('auth.logout')  }}" class="btn btn-sm btn-danger">خروج</a>
     </div>
-    <h2 class="fs-5 mt-2">کاربر {{ auth()->user()->name }} خوش آمدید</h2>
+    <h3 class="fs-5 mt-2  p-2 fw-normal">کاربر <span class="fw-bold">{{ auth()->user()->name }}</span> خوش آمدید</h2>
+    <p class="text-center">نقش: <span>{{ auth()->user()->isAdmin() ? 'مدیر' : 'نویسنده' }}</span></p>
 
 </div>
